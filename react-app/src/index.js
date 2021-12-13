352d2f3e9435ee900dbe45997a70c4c19b13f4f6
DivyeshMakwana12599 <divyeshmakwana12599@gmail.com>
2021-12-14 00:37:00 +0530

    created function for checking Sensor Values

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-const url = 'https://project-cors-fix.herokuapp.com/https://water-treatment-system-api.herokuapp.com'
+const url =
+  'https://project-cors-fix.herokuapp.com/https://water-treatment-system-api.herokuapp.com'
 
 function DataTable() {
   const [data, setData] = useState([])
@@ -25,7 +26,7 @@ function DataTable() {
     <section className='table'>
       <TableHeader />
       {data.map((item, index) => {
-        console.log(item)
+        // console.log(item)
         if (index === data.length - 1) item.className = 'last-item'
         return <TableItem {...item} key={item._id} />
       })}
@@ -46,7 +47,7 @@ function DataTable() {
 }
 
 function Loading({ isLoading }) {
-  console.log(isLoading)
+  // console.log(isLoading)
   if (isLoading) {
     return (
       <>
@@ -56,11 +57,38 @@ function Loading({ isLoading }) {
   }
   return <></>
 }
-function checkState({ temperature }) {
+function checkState({ temperature, orp, ph, turbidity, conductivity }) {
   const FILTERED = ['FILTERED', 'item-green']
   const CONTAMINATED = ['CONTAMINATED', 'item-red']
-  if (temperature[temperature.length - 1] === 30) return FILTERED
-  else return CONTAMINATED
+  if (
+    temperature[temperature.length - 1] !== null &&
+    (temperature[temperature.length - 1] < 30 ||
+      temperature[temperature.length - 1] > 40)
+  )
+    return CONTAMINATED
+  else if (
+    ph[ph.length - 1] !== null &&
+    (ph[ph.length - 1] < 6 || ph[ph.length - 1] > 7)
+  )
+    return CONTAMINATED
+  else if (
+    orp[orp.length - 1] !== null &&
+    (orp[orp.length - 1] < 80 || orp[orp.length - 1] > 100)
+  )
+    return CONTAMINATED
+  else if (
+    turbidity[turbidity.length - 1] !== null &&
+    (turbidity[turbidity.length - 1] < 0.14 ||
+      turbidity[turbidity.length - 1] > 0.2)
+  )
+    return CONTAMINATED
+  else if (
+    conductivity[conductivity.length - 1] !== null &&
+    (conductivity[conductivity.length - 1] < 200 ||
+      conductivity[conductivity.length - 1] > 260)
+  )
+    return CONTAMINATED
+  else return FILTERED
 }
 function TableItem(props) {
   const { city, area, pipeID, sensorValues, className } = props
