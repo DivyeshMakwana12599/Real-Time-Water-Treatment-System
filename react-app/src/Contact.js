d418d66c27bf50708c89ad3f66fe152e96d05d2a
DivyeshMakwana12599 <divyeshmakwana12599@gmail.com>
2022-01-03 03:44:28 +0530

    bugfix for production

--- react-app/src/Contact.js
+++ react-app/src/Contact.js
@@ -45,9 +45,7 @@ function ContactPageForm() {
       console.log('empty values')
     }
   }
-  const a = []
-  const b = 'hello'
-  const temp = a || b
+
   return (
     <>
       <div className='About_container'>
